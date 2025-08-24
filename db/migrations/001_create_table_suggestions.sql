ec8dd0484fff00507ba45ab3b780032f191785ab
happy2btc <58220494+happy2btc@users.noreply.github.com>
2025-08-24 12:14:56 -0700

    Update 001_create_table_suggestions.sql

--- db/migrations/001_create_table_suggestions.sql
+++ db/migrations/001_create_table_suggestions.sql
@@ -1,5 +1,6 @@
 -- 001_create_table_suggestions.sql
 
+-- migrate:up
 CREATE TABLE suggestions (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title TEXT NOT NULL,
@@ -8,3 +9,6 @@ CREATE TABLE suggestions (
   submitted_by TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- migrate:down
+DROP TABLE suggestions;
