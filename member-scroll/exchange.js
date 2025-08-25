d59413a7a4a5570256455aaab5d485f215a68221
happy2btc <58220494+happy2btc@users.noreply.github.com>
2025-08-25 01:58:22 -0700

    Update exchange.js

--- member-scroll/exchange.js
+++ member-scroll/exchange.js
@@ -2,7 +2,7 @@ console.log("exchange.js loaded");
 
 let walletAddress = null;
 
-function connectinitialize() { ... }
+function connectInitialize() {
   walletAddress = "demo_wallet_123";
   document.getElementById("walletStatus").textContent = `Connected: ${walletAddress}`;
   fetchExistingOffering();
