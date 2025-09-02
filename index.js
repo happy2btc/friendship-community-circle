34f0dca5e9f64ed5484db81d5bc5c18827682698
happy2btc <58220494+happy2btc@users.noreply.github.com>
2025-09-02 16:14:24 -0700

    Updating Deploy workflow

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // This assumes you have an App.js component
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
